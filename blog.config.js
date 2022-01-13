3754b8099f66af7519f58d83829fb52602ac7651
ichunhui <hustzch@163.com>
2022-01-13 14:42:35 +0800

    update title

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: '奇怪的文章',
+  title: '格物致知',
   author: 'Jimmy',
   email: 'hustzch@163.com',
   link: 'https://nobelium.vercel.app',
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://github.com/ichunhui',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
